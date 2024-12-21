34675f1a20011689f94ba48550f8da719382bc83
Sam <samllama55@gmail.com>
2024-12-21 12:10:13 +0100

    CHANGE: Update layout to be a bit nicer.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,20 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<header class="flex flex-row place-content-between items-center p-4">
+	<h1 class="text-2xl"><a href="/">Deno Sveltekit</a></h1>
+	<img class="w-32" src="/vite-deno.svg" alt="Vite with Deno" />
+</header>
+
+<main class="container mx-auto p-4">
+	{@render children()}
+</main>
+
+<footer class="my-4 flex flex-row justify-center gap-4">
+	<p class="font-bold">
+		<a href="https://svelte.dev/docs/kit">Sveltekit docs</a>
+	</p>
+	<p class="font-bold">
+		<a href="https://docs.deno.com/">Dino docs</a>
+	</p>
+</footer>
